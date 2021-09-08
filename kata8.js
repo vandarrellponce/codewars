d79d1a27978b82c04f15b2361cd4cc475a351aa6
Van Darrell Ponce <riversideriver@yahoo.com>
2021-09-08 18:21:20 +0800

    kata8 and kata7

---
+++ kata8.js
@@ -0,0 +1,15 @@
+/* Write a function which converts the input string to uppercase. */
+
+function makeUpperCase(str) {
+  return str.toUpperCase()
+}
+
+/* Given a string, write a function that returns the string with 
+a question mark ("?") appends to the end, unless the original string 
+ends with a question mark, in which case, returns the original string.
+"Yes" --> "Yes?" 
+"No?" --> "No?" */
+function ensureQuestion(s) {
+  if (s.includes('?')) return s
+  else return `${s}?`
+}
